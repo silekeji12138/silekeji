362317eebbadf47fb15ce1d19baf37f9f56c5e54
silekeji12138 <34559720+silekeji12138@users.noreply.github.com>
2017-12-22 16:19:56 +0800

    Delete webset.php

--- backend/views/index/webset.php
+++
@@ -1,9 +0,0 @@
-<?php
-$form=\yii\bootstrap\ActiveForm::begin();
-echo $form->field($model,'title')->textInput();
-echo $form->field($model,'keywords')->textInput();
-echo $form->field($model,'describe')->textarea();
-
-echo \yii\helpers\Html::submitButton('提交',['class'=>'btn btn-info']);
-
-yii\bootstrap\ActiveForm::end();
